7d0ad9e1e3a2719504e998d51eed1e0ed3d66f9c
kovalenkoandri <58831202+kovalenkoandri@users.noreply.github.com>
2022-08-04 09:43:09 +0000

    Deploying to gh-pages from @ kovalenkoandri/goit-js-hw-10@05289598945cb0551c1d5598f6fb1a4748f42494 🚀

--- index.841fcf67.js
+++
@@ -1,2 +0,0 @@
-document.querySelector('input[id="search-box"]').addEventListener("input",(function(e){var t;t=e.currentTarget.value,fetch("https://restcountries.com/v3.1/name/".concat(t)).then((function(e){return e.json()})).then(console.log)}));
-//# sourceMappingURL=index.841fcf67.js.map
--- index.cd83f4ae.js
+++
@@ -1,2 +0,0 @@
-document.querySelector('input[id="search-box"]').addEventListener("input",(e=>{var t;t=e.currentTarget.value,fetch(`https://restcountries.com/v3.1/name/${t}`).then((e=>e.json())).then(console.log)}));
-//# sourceMappingURL=index.cd83f4ae.js.map
