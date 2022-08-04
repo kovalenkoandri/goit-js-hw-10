05289598945cb0551c1d5598f6fb1a4748f42494
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-08-04 12:42:24 +0300

    create element img

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,23 +1,55 @@
+const REST_COUNTRIES = `https://restcountries.com/v3.1/name/`;
+const searchParams = new URLSearchParams({
+  fields: 'name,capital,population,flags,languages',
+});
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 export function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v3.1/name/${name}`)
-    .then(r => r.json())
-    .then(console.log);
+  return fetch(
+    `${REST_COUNTRIES}${name}?${searchParams}`
+    // `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => response.json())
+    .then(allcountries => {
+      if (allcountries.length === 1) {
+        const countryFlagThumb = document.createElement('div');
+        const countryFlag = document.createElement('img');
+        const countryName = document.createElement('h2');
+        countryFlagThumb.appendChild(countryFlag);
+        countryName.innerText = allcountries[0].name.official;
+        countryFlag.src = allcountries[0].flags.svg;
+        countryName.style.cssText = `
+        display: inline-block;
+        margin-left: 10px;
+        `;
+        countryFlag.style.cssText = `
+        display: block;
+        height: 100%;
+        width: 100%;
+        object-fit: contain;
+        margin-top: 5px;
+        `;
+        countryFlagThumb.style.cssText = `
+        width: 25px;
+        height: 25px;
+        display: inline-block;
+        `;
+        console.log(allcountries[0].flags);
+        countryInfo.append(countryFlagThumb, countryName);
+      }
+    });
 }
-// const BASE_URL = 'https://pokeapi.co/api/v2';
-
-// function fetchPokemon(pokemonId) {
-//   return fetch(`${BASE_URL}/pokemon/${pokemonId}`).then(response =>
-//     response.json()
-//   );
-// }
-
-// export default { fetchPokemon };
-// You can filter the output of your request to include only the specified fields.
-
-// https://restcountries.com/v2/{service}?fields={field},{field},{field}
-
-// https://restcountries.com/v2/all?fields=name,capital,currencies
-
+// function renderPokemon(pokeData) {
+//   let allPokemonContainer = document.getElementById('poke-container');
+//   let pokeContainer = document.createElement("div") //div will be used to hold the data/details for indiviual pokemon.{}
+//   let pokeName = document.createElement('h4');
+//   pokeName.innerText = pokeData.namelet;
+//   pokeNumber = document.createElement('p')pokeNumber.innerText = `#${pokeData.id}`;
+// let pokeTypes = document.createElement('ul')
+//ul list will hold the pokemon typescreateTypes(pokeData.types, pokeTypes)
+// helper function to go through the types array and create li tags for each onepokeContainer.append(pokeName, pokeNumber, pokeTypes);
+//appending all details to the pokeContainer divallPokemonContainer.appendChild(pokeContainer);
+//appending that pokeContainer div to the main div which will                                                             hold all the pokemon cards}
 // Тебе нужны только следующие свойства:
 
 // - `name.official` - полное имя страны
@@ -25,3 +57,6 @@ export function fetchCountries(name) {
 // - `population` - население
 // - `flags.svg` - ссылка на изображение флага
 // - `languages` - массив языков
+// В ответе от бэкенда возвращаются объекты, большая часть свойств которых тебе не
+// пригодится. Чтобы сократить объем передаваемых данных добавь строку параметров
+// запроса - так этот бэкенд реализует фильтрацию полей.
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import './css/styles.css';
-import {fetchCountries} from './fetchCountries.js';
+import { fetchCountries } from './fetchCountries.js';
+
+var debounce = require('lodash.debounce');
 const input = document.querySelector('input[id="search-box"]');
 const DEBOUNCE_DELAY = 300;
-let log = '';
-input.addEventListener('input', event => {
-  // event.preventDefault();
-  // console.log((/[a-zA-Z]/).test(event.key));
-  // if (event.key >= 'A' && event.key <= 'z' && event.key !== 'Backspace') {
-    // log += event.key;
-    fetchCountries(event.currentTarget.value);
-    // fetchCountries(log.trim());
-  // }
-});
+input.addEventListener('input', debounce(() => {
+    if (input.value === '') return;
+    fetchCountries(input.value.trim());
+},300));
