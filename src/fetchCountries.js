37e00915fe9cc67ad347e59434b57a328a0146e3
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-08-07 02:11:56 +0300

    catch errors

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,24 +1,17 @@
-import { allcountriesLengthEqualToOne } from './allcountriesLengthEqualToOne';
-import { allcountriesLengthEqualFromTwoToTen } from './allcountriesLengthEqualFromTwoToTen';
-import { allcountriesLengthMoreThenTen } from './allcountriesLengthMoreThenTen';
 import { allcountriesStatusIsEqualTo404 } from './allcountriesStatusIsEqualTo404';
 const REST_COUNTRIES = `https://restcountries.com/v3.1/name/`;
 const searchParams = new URLSearchParams({
   fields: 'name,capital,population,flags,languages',
 });
 export function fetchCountries(name) {
-  return fetch(
-    `${REST_COUNTRIES}${name}?${searchParams}`
-  )
+  return fetch(`${REST_COUNTRIES}${name}?${searchParams}`)
     .then(response => {
       console.log(response);
-      return response.json();
+      if (response.ok) return response.json();
+      allcountriesStatusIsEqualTo404();
+      throw new Error('Error fetching data');
     })
-    .then(allcountries => {
-      console.log(allcountries);
-      if (allcountries.length === 1) allcountriesLengthEqualToOne(allcountries);
-      if (allcountries.length >= 2 && allcountries.length <= 10) allcountriesLengthEqualFromTwoToTen(allcountries);
-      if (allcountries.length > 10) allcountriesLengthMoreThenTen();
-      if (allcountries.status === 404) allcountriesStatusIsEqualTo404();
+    .catch(error => {
+      console.log('Error: ', error);
     });
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries.js';
-
+import { refs } from './refs';
+import { allcountriesLengthEqualToOne } from './allcountriesLengthEqualToOne';
+import { allcountriesLengthEqualFromTwoToTen } from './allcountriesLengthEqualFromTwoToTen';
+import { allcountriesLengthMoreThenTen } from './allcountriesLengthMoreThenTen';
 var debounce = require('lodash.debounce');
 const input = document.querySelector('input[id="search-box"]');
 const DEBOUNCE_DELAY = 300;
 input.addEventListener(
   'input',
   debounce(() => {
-    if (input.value === '') return;
-    fetchCountries(input.value.trim());
+    if (input.value === '') {
+      refs.countryList.innerHTML = '';
+      refs.countryInfo.innerHTML = '';
+      return;
+    }
+    fetchCountries(input.value.trim()).then(allcountries => {
+      console.log(allcountries);
+      if (allcountries !== undefined) {
+        if (allcountries.length === 1)
+          allcountriesLengthEqualToOne(allcountries);
+        if (allcountries.length >= 2 && allcountries.length <= 10)
+          allcountriesLengthEqualFromTwoToTen(allcountries);
+        if (allcountries.length > 10) allcountriesLengthMoreThenTen();
+      }
+    });
   }, DEBOUNCE_DELAY)
 );
